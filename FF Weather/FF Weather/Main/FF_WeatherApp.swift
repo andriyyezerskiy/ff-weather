1f944a764a9b642f488f84257b6beae1e20498d5
Andriy Yezerskiy <andriyyezerskiy@icloud.com>
2023-02-26 10:59:23 +0100

    Project setup

---
+++ FF Weather/FF Weather/Main/FF_WeatherApp.swift
@@ -0,0 +1,18 @@
+//
+//  FF_WeatherApp.swift
+//  FF Weather
+//
+//  Created by Andriy Yezerskiy on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FF_WeatherApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+			Text("")
+        }
+    }
+}
